4dabb5b72bee8676260d323986dbeaea0928ea5c
bir-ham <birhanuh@gmail.com>
2014-09-15 23:36:05 +0300

    Removed vendor, test, lib directories. Prepared setup for database.yml file. Added/edited Gemfile. Included high level integration test. Added features folder to spec.

---
+++ spec/features/account_creation_feature_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe 'account creation' do
+  it 'allows user to create account' do
+    visit root_path
+    click_link 'Create Account'
+
+    fill_in 'First Name', with: 'Birhanu'
+    fill_in 'Second Name', with: 'Hailemariam'
+    fill_in 'Email', with: 'birhanu@gmail.com'
+    fill_in 'Password', with: 'pw'
+    fill_in 'Password confirmation', with: 'pw'
+    fill_in 'Subdomain', with: 'test_subdomain'
+    click_button 'Create Account'
+
+    expect(page).to have_content('Signed up successfully')
+  end
+end
