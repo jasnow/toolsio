536c268ade62bfc3a462f150ec59a3a052b2c789
birhanuh <birhanuh@gmail.com>
2015-04-28 20:10:14 +0300

    Update rails version to 4.2.1 and devise to 3.4.0

--- Gemfile
+++ Gemfile
@@ -1,8 +1,10 @@
 source 'https://rubygems.org'
+source 'http://gems.github.com'
+ruby '2.2.0'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.1.6'
+gem 'rails', '4.2.1'
 gem 'pg'
 
 gem 'sass-rails', '~> 4.0.3'
@@ -10,7 +12,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 
-gem 'devise', '~> 3.2.0'
+gem 'devise', '~> 3.4.0'
 gem 'devise_invitable', '~> 1.3.4'
 
 gem 'apartment'
--- db/schema.rb
+++ db/schema.rb
@@ -16,14 +16,14 @@ ActiveRecord::Schema.define(version: 20150219173829) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "accounts", force: true do |t|
+  create_table "accounts", force: :cascade do |t|
     t.string   "subdomain"
     t.integer  "owner_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "projects", force: true do |t|
+  create_table "projects", force: :cascade do |t|
     t.string   "name"
     t.string   "client"
     t.boolean  "archived",   default: false, null: false
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20150219173829) do
     t.datetime "updated_at"
   end
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "second_name"
     t.string   "email",                  default: "", null: false
