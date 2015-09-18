c92151f83972b426eede5621ffcd6368fa3f86fb
birhanuh <birhanuh@gmail.com>
2015-09-18 20:44:18 +0300

    Add trackable devise module on user model. Uncommit trackable fields on devise migration file.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :invitable, :database_authenticatable, :recoverable, :rememberable, :validatable
+  devise :invitable, :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 
   validates :first_name, presence: true
   validates :second_name, presence: true
