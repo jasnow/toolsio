7bb6d7b1598730bb7797313e0f1e55425d22e684
birhanuh <birhanuh@gmail.com>
2015-09-18 18:03:20 +0300

    Fix indentations on spec files.

--- spec/features/authentication_feature_spec.rb
+++ spec/features/authentication_feature_spec.rb
@@ -16,19 +16,19 @@ describe 'user authentication' do
 	end	
 
 	it 'does not allow user to sign in unless on subdomain' do
-    	expect { visit new_user_session_path }.to raise_error ActionController::RoutingError
-  	end
+    expect { visit new_user_session_path }.to raise_error ActionController::RoutingError
+  end
 
-  	it 'does not allow user from one subdomain to sign in on another subdomain' do
-	    user2 = build(:user)
-	    account2 = create(:account_with_schema, owner: user2)
+  it 'does not allow user from one subdomain to sign in on another subdomain' do
+	  user2 = build(:user)
+	  account2 = create(:account_with_schema, owner: user2)
 
-	    sign_user_in(user2, subdomain: account2.subdomain)
-	    expect(page).to have_content('Signed in successfully')
+	  sign_user_in(user2, subdomain: account2.subdomain)
+	  expect(page).to have_content('Signed in successfully')
 
-	    sign_user_in(user2, subdomain: account.subdomain)
-	    expect(page).to have_content('Invalid email or password')  
-  	end
+	  sign_user_in(user2, subdomain: account.subdomain)
+	  expect(page).to have_content('Invalid email or password')  
+  end
 
 	it 'allows user to sign out' do
 		sign_user_in(user, subdomain: account.subdomain)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  	describe 'validations' do 
-  		it { should validate_presence_of(:first_name) }
-  		it { should validate_presence_of(:second_name) }
-	    it { should validate_presence_of(:email) }
-	    it { should validate_presence_of(:password) }
+  describe 'validations' do 
+  	it { should validate_presence_of(:first_name) }
+  	it { should validate_presence_of(:second_name) }
+	  it { should validate_presence_of(:email) }
+	  it { should validate_presence_of(:password) }
 	end	
 
 	describe 'associations' do
