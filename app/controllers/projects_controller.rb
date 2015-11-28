c05b025159f4b7ee6262802115c4b39f07ac585a
bir-ham <birhanuh@gmail.com>
2015-11-28 17:15:18 +0200

    Add key-value properties.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,41 +1,41 @@
 class ProjectsController < ApplicationController
-  
-	def index
-		@projects = Project.all
-	end
 
-	def new
-		@project = Project.new
-	end
+  def index
+    @projects = Project.all
+  end
 
-	def create
-		@project = Project.new(project_params)
-		if @project.save
-			redirect_to root_path, notice: "Project created!"
-		else
-			render :new
-		end		
-	end
+  def new
+    @project = Project.new
+  end
 
-	def edit
-		@project = Project.find(params[:id])
-	end
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to root_path, notice: I18n.t('flash.notice.project_created')
+    else
+      render :new
+    end
+  end
 
-	def update
-		@Project = Project.find(params[:id])
+  def edit
+    @project = Project.find(params[:id])
+  end
 
-		if @Project.update(project_params)
-			redirect_to root_path, notice: "Project updated!"
-		else
-			render :edit
-		end	
+  def update
+    @Project = Project.find(params[:id])
 
-	end
+    if @Project.update(project_params)
+      redirect_to root_path, notice: I18n.t('flash.notice.project_updated')
+    else
+      render :edit
+    end
 
-	private 
+  end
 
-	def project_params
-		params.require(:project).permit(:name, :client, :archived)
-	end
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :client, :archived)
+  end
 
 end
