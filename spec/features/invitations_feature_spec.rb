e5eb8aff3a61d338edb8b1ad4b31f6a804f91535
bir-ham <birhanuh@gmail.com>
2016-02-28 22:06:37 +0200

    Change user.name call to user.first_name and user.second_name.

--- spec/features/invitations_feature_spec.rb
+++ spec/features/invitations_feature_spec.rb
@@ -4,8 +4,8 @@ describe 'invitations' do
   let!(:account) { create(:account_with_schema) }
   let(:user) { account.owner }
 
-  before do 
-  	set_subdomain(account.subdomain) 
+  before do
+  	set_subdomain(account.subdomain)
   	sign_user_in(user)
   	visit users_path
   end
@@ -16,27 +16,28 @@ describe 'invitations' do
     expect(page).to have_content 'Send Invitation'
     expect(page).to have_content 'invalid'
   end
-    
+
   it 'shows the owner in the authorized users list' do
-   	expect(page).to have_content user.name
+   	expect(page).to have_content user.first_name
+    expect(page).to have_content user.second_name
   	expect(page).to have_content user.email
-  	expect(page).to have_content '.glyphicon-ok'
-  end	
+  	expect(page).to have_css '.glyphicon-ok'
+  end
 
   describe 'when user is invited' do
-    before do      
+    before do
       fill_in 'Email', with: 'birhanuh@bizdesk.com'
       click_button 'Invite User'
-    end  
+    end
 
-    it 'shows invitation when user is invited' do 
+    it 'shows invitation when user is invited' do
     	expect(page).to have_content 'invitation email has been sent'
     	expect(page).to have_content 'birhanuh@bizdesk.com'
     	expect(page).to have_content 'Invitation Pending'
     end
 
     context 'user accepts invitation' do
-      before do    
+      before do
         click_link 'Sign out'
 
         open_email 'birhanuh@bizdesk.com'
@@ -47,17 +48,17 @@ describe 'invitations' do
         fill_in 'Password confirmation', with: 'pw'
         click_button 'Create Account'
       end
-      
-      it 'confirms account creation' do   
-        expect(page).to have_content 'Your account was created successfuly'
-      end  
 
-      it 'shows a check mark on the user page' do 
+      it 'confirms account creation' do
+        expect(page).to have_content 'Your account was created successfuly'
+      end
+
+      it 'shows a check mark on the user page' do
         visit users_path
         within('tr', text: 'Birhanu') do
           expect(page).to have_content '.glyphicon-ok'
-        end  
-      end  
-    end	
-  end  
+        end
+      end
+    end
+  end
 end
