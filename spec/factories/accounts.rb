2e9713123ec59ee8cc919ac0e7bbbe2cf4b9ac60
birhanuh <birhanuh@gmail.com>
2016-02-29 20:12:44 +0200

    Fix confilicts.

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -1,6 +1,6 @@
-FactoryGirl.define do  
+FactoryGirl.define do
   sequence(:subdomain) { |n| "subdomain#{n}" }
-  
+
 	factory :account do
   	sequence(:subdomain) { |n| "subdomain#{n}" }
   	association :owner, factory: :user, strategy: :build
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do
   	first_name 'Birhanu'
   	second_name 'Hailemariam'
-  	sequence(:email) { |n| "email#{n}gmail.com"}
-  	password 'pw' 
+    sequence(:email) { |n| "email#{n}@example.com" }
+  	password 'pw'
   end
 end
