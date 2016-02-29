374c7121c4d9d38b733a47e46b946782f01ffb60
birhanuh <birhanuh@gmail.com>
2016-02-29 20:05:31 +0200

    Add validation and association describe blocks.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219173829) do
+ActiveRecord::Schema.define(version: 20160220165305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20150219173829) do
     t.integer  "owner_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "invoices", force: :cascade do |t|
+    t.datetime "date"
+    t.string   "company"
+    t.decimal  "tax"
+    t.string   "salesperson"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "projects", force: :cascade do |t|
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :company }
+    it { should validate_presence_of :salesperson }
+    it { should validate_presence_of :tax }
+    it { should validate_presence_of :date }
+
+    it { should allow_value('company').for(:company) }
+    it { should allow_value('salesperson').for(:salesperson) }
+    it { should allow_value('23.7').for(:tax) }
+    it { should allow_value(new Date).for(:date) }
+
+    it { should_not_allow_value('seven').for(:tax) }
+    it { should_not_allow_value('today').for(:date) }
+  end
+
+  describe 'associations' do
+    it 'Should have owner'
+  end  
 end
