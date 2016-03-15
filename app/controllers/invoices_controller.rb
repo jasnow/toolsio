e5688a5bc27f2b7e3fd4cc073ecf93e274f57916
birhanuh <birhanuh@gmail.com>
2016-03-15 20:43:09 +0200

    Add _form & new template. Implement controller actions.

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,19 +1,30 @@
-class InvoiceController < AplicationController 
+class InvoicesController < ApplicationController 
   
   def index
-
+    @invoices = Invoice.all
   end
 
   def new
-
+    @invoice = Invoice.new
   end
 
-  def show
-
+  def create
+    @invoice = Invoice.new(invoice_params)
+    if @invoice.save
+      redirect_to invoices_path, notice: 'Invoice created!'
+    else
+      render :new
+    end  
   end
 
   def destroy
 
   end
+
+  private
+
+  def invoice_params
+    params.require(:invoice).permit(:company, :salesperson, :tax, :date)
+  end  
 
 end
