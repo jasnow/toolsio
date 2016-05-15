a4731c00ebd4d6a4e128f1e891a6109794fb08fe
bir-ham <birhanuh@gmail.com>
2016-05-15 21:13:36 +0300

    Put empty? conditoinal clause. Re-style header-nav li on active & on focus state. Update .gitignore file.

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -3,10 +3,10 @@ module LayoutHelper
     @layout_flash = opts.fetch(:layout_flash) { true }
 
     capture do
-      flash.each do |name, msg|        
+      flash.each do |name, msg|
         concat(content_tag(:div, msg, class: "alert alert-#{name} alert-dismissible", role: "alert") do
           concat content_tag(:button, content_tag(:span, '&times;'.html_safe, aria: { hidden: 'true' }), class: 'close', data: { dismiss: 'alert' }, aria: { label: 'Close' })
-          concat content_tag(:p, msg)
+          concat content_tag(:span, msg)
         end)
       end
     end
@@ -18,18 +18,18 @@ module LayoutHelper
 
   def controller_name?(controller_name)
     return true if controller.controller_name == controller_name
-  end  
+  end
 
   def action_name?(action_name)
     return true if controller.action_name == action_name
-  end  
+  end
 
   def is_footer_visible
-    first_condition = controller_name != "sessions" && (controller_name != "accounts" && action_name != "create" ) && 
+    first_condition = controller_name != "sessions" && (controller_name != "accounts" && action_name != "create" ) &&
     (controller_name != "invitaions" && action_name != "edit" )
 
     first_condition
 
   end
-  
+
 end
