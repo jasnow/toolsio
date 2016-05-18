294a95842d7b7342e1efb4a011c620a5bc30ddd6
Birhanu Hailemariam <birhanu.hailemariam@gmail.com>
2016-05-18 21:29:48 +0300

    Add presence: ture validator.

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,10 +1,10 @@
 class Invoice < ActiveRecord::Base
   validates :customer, presence: true
   validates :salesperson, presence: true
-  validates :interest_on_arrears, presence: true
   validates :date_of_an_invoice, presence: true
-  validates :deadline, presence: false, allow_nil: true
-  validates :payment_term, presence: false, allow_nil: true
+  validates :deadline, presence: true, allow_nil: true
+  validates :payment_term, presence: true, allow_nil: true
+  validates :interest_on_arrears, numericality: true, allow_nil: true
   validates :reference_number, presence: true, 
     uniqueness: true,
     numericality: { only_integer: true }
@@ -14,10 +14,10 @@ class Invoice < ActiveRecord::Base
   validate :choose_xor_date
 
   private
-    def choose_xor_date
-      unless deadline.blank? ^ payment_term.blank?
-        errors.add(:base, "Specify a deadline or a payment term, not both")
-      end
-    end  
+  def choose_xor_date
+    unless deadline.blank? ^ payment_term.blank?
+      errors.add(:base, 'specify a deadline or a payment term. Not both empty, nor both filled')        
+    end
+  end  
 
 end
