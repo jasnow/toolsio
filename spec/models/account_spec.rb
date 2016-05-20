c462c70250fba17013881d4cf8ca2c4099ea4b76
Al Snow <jasnow@hotmail.com>
2016-05-20 07:58:04 -0400

    Feedback Code

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -4,8 +4,7 @@ RSpec.describe Account, :type => :model do
   describe 'validations' do
     it { should validate_presence_of :owner }
     it { should validate_presence_of :subdomain }
-    it { should validate_uniqueness_of :subdomain }
-
+    it { should validate_uniqueness_of(:subdomain).case_insensitive }
     it { should allow_value('birhanu').for(:subdomain) }
     it { should allow_value('test').for(:subdomain) }
 
